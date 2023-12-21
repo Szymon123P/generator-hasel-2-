df627acca53281b5ab6bbcc3d797ca7e106f3134
Szymon123P <154538478+Szymon123P@users.noreply.github.com>
2023-12-21 19:32:30 +0100

    Add files via upload

---
+++ podaj długość hasła.py
@@ -0,0 +1,20 @@
+import random
+
+# pierwszy program: Generator haseł
+
+losoweCyfryIZnaki = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+licznik = -1
+
+for i in losoweCyfryIZnaki:
+    licznik += 1
+
+# print(licznik)
+
+dlogoscHasla = int(input("Podaj długość hasła"))
+haslo = ""
+
+for i in range(dlogoscHasla):
+    haslo += losoweCyfryIZnaki[random.randint(0, licznik)]
+
+print(haslo)
